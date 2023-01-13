88f357f6eb035dc03227afc081f74d3ffc30b383
Lasal Jayawardena <61999541+LasalJayawardena@users.noreply.github.com>
2023-01-14 02:20:15 +0530

    Delete Emotion_Detection_in_Text directory

--- Emotion_Detection_in_Text/API/api_tester.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-POST_URL = "http://localhost:83/get_text_analysis"
-
-
-params = {'text': 'I am feeling happy', 'depression':"true", "emotion":"true", "suicide":"true"}
-
-r = requests.post(POST_URL, params=params )
-print(r.content)
